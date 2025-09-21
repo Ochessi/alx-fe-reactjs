d394ff126a4da6b31d93087752d82ceed4751369
Ochessi <ochessibrian@gmail.com>
2025-09-21 21:22:03 +0300

    Display Search Results

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -240,7 +240,7 @@ const Search = () => {
           <svg className="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
-          <h3 className="mt-2 text-sm font-medium text-gray-900">No users found</h3>
+          <h3 className="mt-2 text-sm font-medium text-gray-900">Looks like we cant find the user</h3>
           <p className="mt-1 text-sm text-gray-500">Try adjusting your search criteria.</p>
         </div>
       )}
